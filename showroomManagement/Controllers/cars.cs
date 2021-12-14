0765a07e8d81da131acc8c577fdb828b19f923f9
MuhammadUmair161 <79800656+MuhammadUmair161@users.noreply.github.com>
2021-12-15 01:43:44 +0500

    add dashboard

---
+++ showroomManagement/Controllers/cars.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace showroomManagement.Controllers
+{
+    public class cars : Controller
+    {
+        // GET: cars
+        public ActionResult newCar()
+        {
+            return View();
+        }
+        public ActionResult CarPv()
+        {
+            return View();
+        }
+
+        // GET: cars/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: cars/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: cars/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: cars/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: cars/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: cars/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

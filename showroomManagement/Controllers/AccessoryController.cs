6e168d6f8df78d3a29d8ca8cb34cf6ef0d87d188
MuhammadUmair161 <79800656+MuhammadUmair161@users.noreply.github.com>
2021-12-18 16:06:03 +0500

    Add _Accessory Controller+view

---
+++ showroomManagement/Controllers/AccessoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using showroomManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace showroomManagement.Controllers
+{
+
+    public class AccessoryController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ShrowroomDbContext _context;
+        public AccessoryController(ShrowroomDbContext context, IWebHostEnvironment WebHostEnvironment)
+        {
+            this._context = context;
+            this._webHostEnvironment = WebHostEnvironment;
+        }
+        public IActionResult AccessoryIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult _Accessory(Accessory accessory)
+        {
+            if (ModelState.IsValid)
+            {
+                this._context.Accessories.Add(accessory);
+                if (this._context.SaveChanges() > 0)
+                {
+                    return RedirectToAction("AccessoryIndex", "Accessory");
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult _AccessoriesStock(AccessoriesStock accessoriesStock)
+        {
+            if (ModelState.IsValid)
+            {
+                this._context.AccessoriesStocks.Add(accessoriesStock);
+                if (this._context.SaveChanges() > 0)
+                {
+                    return RedirectToAction("AccessoryIndex", "Accessory");
+                }
+            }
+            return View();
+        }
+
+    }
+}
